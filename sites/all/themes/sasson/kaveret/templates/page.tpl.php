040c03b6b27252b26414d5d8b6585bdab188105d
Aron Stein <ashtein@linnovate.net>
2012-10-24 13:14:23 +0200

    css for activites

--- sites/all/themes/sasson/kaveret/templates/page.tpl.php
+++ sites/all/themes/sasson/kaveret/templates/page.tpl.php
@@ -92,6 +92,9 @@
 
 <?php if ($page['featured']): ?>
     <div id="featured">
+<?php if ($title): ?>
+      <h1 class="title" id="page-title"><?php print $title; ?></h1>
+    <?php endif; ?>
       <?php print render($page['featured']); ?>
     </div><!-- /#featured -->
   <?php endif; ?>
@@ -118,9 +121,6 @@
      
     <?php print $breadcrumb; ?>
     <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1 class="title" id="page-title"><?php print $title; ?></h1>
-    <?php endif; ?>
     <?php print render($title_suffix); ?>
     <?php if ($tabs): ?>
       <div class="tabs"><?php print render($tabs); ?></div>
