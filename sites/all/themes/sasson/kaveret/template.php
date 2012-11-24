909419d62c8f17794bae6d609647dd9a2a0063a7
root <root@li305-75.members.linode.com>
2012-11-24 18:05:00 +0000

    Addign modified and missing files

--- sites/all/themes/sasson/kaveret/template.php
+++ sites/all/themes/sasson/kaveret/template.php
@@ -1,2 +1,7 @@
 ﻿<?php
+    function kaveret_block_view_alter(&$data, $block) {
+    if($block->title != '<none>') { // condition, so all block titles won't be translated
+    $block->title = t($block->title);
+    }
+    }
 
