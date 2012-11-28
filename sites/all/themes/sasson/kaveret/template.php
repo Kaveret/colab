c729820332f0d3669479ea704ff8665b0fbadb09
Lea Amend <lea.a@linnovate.net>
2012-11-28 14:53:42 +0200

    testing lea commit

--- sites/all/themes/sasson/kaveret/template.php
+++ sites/all/themes/sasson/kaveret/template.php
@@ -4,4 +4,15 @@
     $block->title = t($block->title);
     }
     }
+    function YOURTHEME_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+   
+   
+    $form['search_block_form']['#default_value'] = t('Searchdefault'); // Set a default value for the textfield
+    $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
+  
+  }
+} 
+?>
 
