119e220472ce82206c972b2b8bddc75ee66cb653
Matthew Slater <matslats@gmail.com>
2012-12-04 16:05:59 +0000

    added groups summary views to front page

--- profiles/likeabee/themes/beezy/template.php
+++ profiles/likeabee/themes/beezy/template.php
@@ -1,5 +1,8 @@
 <?php
-
-function beezy_process_page(&$vars) {
-
+/*
+function beezy_preprocess_page(&$vars) {
+  $vars['secondary_menu_links'] = theme('links', array(
+    'links' => variable_get('menu_secondary_links_source', 'footer-menu')
+  ));
 }
+*/
