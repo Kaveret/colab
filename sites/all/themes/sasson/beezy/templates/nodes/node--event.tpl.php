3424b98edf9fec8224b9ac4c02c7217e5f82d033
Matthew Slater <matslats@gmail.com>
2013-01-03 16:51:17 +0200

    removed seven theme and improved templating for beezy

--- sites/all/themes/sasson/beezy/templates/nodes/node--event.tpl.php
+++ sites/all/themes/sasson/beezy/templates/nodes/node--event.tpl.php
@@ -76,9 +76,50 @@
  * @see template_process()
  */
 ?>
-?>
-<div class = "node node-event teaser">
-<a href="/node/<?php print $nid; ?>"><?php print $title; ?></a>
-<a href="/node/<?php print $nid; ?>"><?php print $image; ?></a>
-<a href="/node/<?php print $nid; ?>"><?php print $body; ?></a>
-</div><!-- /event teaser -->
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if ($user_picture || !$page || $display_submitted): ?>
+    <header>
+      <?php print $user_picture; ?>
+
+      <?php print render($title_prefix); ?>
+      <?php if (!$page): ?>
+        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+
+      <?php if ($display_submitted): ?>
+        
+      <p class="submitted">
+        <?php print $submitted; ?>
+        <time pubdate datetime="<?php print $submitted_pubdate; ?>">
+        <?php print $submitted_date; ?>
+        </time>
+      </p>
+        
+      <?php endif; ?>
+    </header>
+  <?php endif; ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+      <?php if ($page): ?>
+	<h2><?php print $title; ?></h2>
+      <?php endif; ?>
+    <?php
+      // We hide the comments, tags and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      hide($content['field_tags']);
+      print render($content);
+    ?>
+  </div><!-- /.content -->
+
+  <?php if (!empty($content['field_tags']) || !empty($content['links'])): ?>
+    <footer>
+      <?php print render($content['field_tags']); ?>
+      <?php print render($content['links']); ?>
+    </footer>
+  <?php endif; ?>
+
+  <?php print render($content['comments']); ?>
+
+</article><!-- /.node -->
