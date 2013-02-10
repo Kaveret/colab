ea3f79bbb13143fa13e1cdcc106699216cbb635a
Amitaibu <amitai@gizra.com>
2013-02-10 12:04:46 +0200

    Minor fix to installation script.

--- default.install.sh
+++ default.install.sh
@@ -8,12 +8,12 @@ bash scripts/build
 
 cd www
 
-cp sites/default/default.settings.php sites/default/settings.php
 mkdir sites/default/files
+chmod -R 777 sites/default/files
 
 drush si -y kaveret --account-pass=admin --db-url=mysql://root@localhost/kaveret --db-su=root -v
-drush en migrate_ui migrate_extras
+drush en migrate_ui migrate_extras -y
 drush mi --all --user=1
 
-chmod -R 777 sites/default/files
+
 
