b890e0994f149583b0f24c18189cbadd17f1cf84
Amitaibu <amitai@gizra.com>
2013-02-10 11:51:07 +0200

    default.install.sh

--- default.install.sh
+++ default.install.sh
@@ -9,7 +9,8 @@ cd www
 cp sites/default/default.settings.php sites/default/settings.php
 mkdir sites/default/files
 
-drush si -y cadaboo --account-pass=admin --db-url=mysql://root@localhost/cadaboo --db-su=root -v
+drush si -y kavertet --account-pass=admin --db-url=mysql://root@localhost/kavertet --db-su=root -v
+drush en migrate_ui migrate_extras
 drush mi --all --user=1
 
 chmod -R 777 sites/default/files
