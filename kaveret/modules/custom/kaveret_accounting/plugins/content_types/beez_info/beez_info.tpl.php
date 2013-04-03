110275bc55ece7856c75e61bd7f15a81203a3109
Shlomi <shlomi@shlomi-PC.(none)>
2013-04-03 14:35:31 +0300

    Don't show graph.

--- kaveret/modules/custom/kaveret_accounting/plugins/content_types/beez_info/beez_info.tpl.php
+++ kaveret/modules/custom/kaveret_accounting/plugins/content_types/beez_info/beez_info.tpl.php
@@ -2,7 +2,9 @@
   <div class="info">
     <?php print $info; ?>
   </div>
-  <div class="graph">
-    <?php print $graph; ?>
-  </div>
+  <?php if($graph): ?>
+    <div class="graph">
+      <?php print $graph; ?>
+    </div>
+  <?php endif; ?>
 </div>
