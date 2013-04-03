868894f1c45dc159eda3b82432e26ce03a2a5dcd
Amitaibu <amitai@gizra.com>
2013-04-03 14:37:48 +0300

    #108: My Beez graph info

---
+++ kaveret/modules/custom/kaveret_accounting/plugins/content_types/beez_info/beez_info.tpl.php
@@ -0,0 +1,10 @@
+<div class="clearfix">
+  <div class="info">
+    <?php print $info; ?>
+  </div>
+  <?php if($graph): ?>
+    <div class="graph">
+      <?php print $graph; ?>
+    </div>
+  <?php endif; ?>
+</div>
