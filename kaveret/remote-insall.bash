5ce1907cc0566749671a0fb8c327bc9e5cd071dd
Amitaibu <amitai@gizra.com>
2013-04-07 12:10:21 +0300

    Create dummy content on remote-install.

--- kaveret/remote-insall.bash
+++ kaveret/remote-insall.bash
@@ -4,3 +4,4 @@
 drush @pantheon.hakaveret.dev sql-drop -y
 drush @pantheon.hakaveret.dev si -y kaveret --account-pass=admin
 drush @pantheon.hakaveret.dev mi --all --user=1
+drush @pantheon.hakaveret.dev genc 100 --types=offer,request --skip-fields=field_status
